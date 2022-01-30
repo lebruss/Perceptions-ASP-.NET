d288a8c9d8738aef17249a5e682f0e3631756ae8
Caleb <Caleb@DESKTOP-Q0CJAF6>
2022-01-30 18:54:54 +0200

    Add project files.

---
+++ Models/Person.cs
@@ -0,0 +1,15 @@
+﻿namespace Perceptions.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public Person()
+        {
+
+        }
+    }
+}
